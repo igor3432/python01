7284567881f27eadb4e65549b58f3373d238c5d8
igor3432 <137342044+igor3432@users.noreply.github.com>
2025-06-14 18:13:36 -0300

    Em andamento ...

---
+++ aula016/ex016.py
@@ -0,0 +1,11 @@
+import math 
+num = int(input('Digite um numero: '))
+raiz = math.sqrt(num)
+print('Á raiz de {:.2f} é igual a {:.2f}'.format(num,raiz))
+print('='*100)
+
+#rendom
+import random
+num = random.randint(1, 10)
+print(num)
+print('='*100)
--- aula017/ex017.py
+++ aula017/ex017.py
@@ -1,9 +1,3 @@
-import math 
-num = int(input('Digite um numero: '))
-raiz = math.sqrt(num)
-print('Á raiz de {:.2f} é igual a {:.2f}'.format(num,raiz))
-
-#rendom
-import random
-num = random.randint(1, 10)
-print(num)
+from math import trunc
+num = float(input('Digite um numero: '))
+print('O valor ')
